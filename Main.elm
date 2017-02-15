005b1eb63964aec520bc2a6427a271b072486b5f
Ygor <ygor.bruxel@gmail.com>
2017-02-15 20:53:20 -0200

    Make a square on mouse up

--- Main.elm
+++ Main.elm
@@ -1,42 +1,72 @@
 module Main exposing (..)
 
-import Mouse exposing (Position)
 import Html exposing (Html, div, text, program)
+import Html.Attributes exposing (style)
+import Mouse exposing (Position)
 
 
 type Msg
     = Moved Position
+    | Up Position
 
 
 type alias Model =
-    Maybe Position
+    { current : Maybe Position
+    , lastUp : Maybe Position
+    }
 
 
 init : ( Model, Cmd msg )
 init =
-    ( Nothing, Cmd.none )
+    ( Model Nothing Nothing, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Moved position ->
-            ( Just position, Cmd.none )
+            ( { model | current = Just position }, Cmd.none )
+
+        Up position ->
+            ( { model | lastUp = Just position }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Mouse.moves Moved
+    Sub.batch [ Mouse.moves Moved, Mouse.ups Up ]
 
 
 view : Model -> Html Msg
 view model =
-    case model of
+    case model.current of
         Nothing ->
             text "Mouse hasn't moved yet!"
 
         Just position ->
-            text ("X:" ++ (toString position.x) ++ "Y:" ++ (toString position.y))
+            div []
+                [ text ("X:" ++ (toString position.x) ++ "Y:" ++ (toString position.y))
+                , lastUpCircle model
+                ]
+
+
+lastUpCircle : Model -> Html msg
+lastUpCircle model =
+    case model.lastUp of
+        Nothing ->
+            div [] []
+
+        Just position ->
+            div
+                [ style
+                    [ ( "position", "fixed" )
+                    , ( "background-color", "red" )
+                    , ( "width", "100px" )
+                    , ( "height", "100px" )
+                    , ( "top", (toString position.y) ++ "px" )
+                    , ( "left", (toString position.x) ++ "px" )
+                    ]
+                ]
+                []
 
 
 main =
