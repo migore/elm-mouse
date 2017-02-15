7da79d49585d21166401fd8e5d23dc942e768c64
Ygor <ygor.bruxel@gmail.com>
2017-02-15 20:08:51 -0200

    TEA boilerplate

--- Main.elm
+++ Main.elm
@@ -1,7 +1,40 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, div, text, program)
+
+
+type Msg
+    = NoOp
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd msg )
+init =
+    ( "Hello", Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    text model
 
 
 main =
-    text "Hello World!!!!"
+    program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
